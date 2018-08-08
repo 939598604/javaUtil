cdfa897e1b329ceeda73422410ed70a9abfd2b86
939598604 <939598604@qq.com>
2018-08-08 09:17:19 +0800

    Initial commit

---
+++ src/main/java/org/jinhua/chen/App.java
@@ -0,0 +1,13 @@
+package org.jinhua.chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
